13d6d0321266e6e0e22d78ae5eb31cb08c1487b4
mannatgupta146 <mannatgupta146@gmail.com>
2026-04-02 02:15:32 +0530

    added filters

---
+++ script.js
@@ -0,0 +1,72 @@
+const filters = {
+    brightness: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: '%'
+    },
+    contrast: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: '%'
+    },
+    saturation: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: '%'
+    },
+    exposure: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: '%'
+    },
+    saturation: {
+        value: 0,
+        min: 0,
+        max: 200,
+        unit: '%'
+    },
+    hueRotation: {
+        value: 0,
+        min: 0,
+        max: 360,
+        unit: 'deg'
+    },
+    blur: {
+        value: 0,
+        min: 0,
+        max: 20,
+        unit: 'px'
+    },    
+    grayscale: {
+        value: 0,
+        min: 0,
+        max: 200,
+        unit: '%'
+    },
+    sepia: {
+        value: 0,
+        min: 0,
+        max: 200,
+        unit: '%'
+    },
+    invert: {
+        value: 0,
+        min: 0,
+        max: 200,
+        unit: '%'
+    },
+    opacity: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: '%'
+    }
+}
+
+function createFilterElement(name, unit = "%", value, min, max){
+
+}
